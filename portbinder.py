ad07241891946ab6d9ccc6d5fdab7e478f837aff
admin <gimbaltest123@hotmail.com>
2024-07-17 07:11:05 +0800

    initial commit

---
+++ portbinder.py
@@ -0,0 +1,29 @@
+import serial
+import threading
+
+# Configuration for the serial ports
+port1 = '/dev/ttyUSB0'
+port2 = '/dev/ttyUSB1'
+baud_rate = 9600
+
+# Initialize serial ports
+ser1 = serial.Serial(port1, baud_rate)
+ser2 = serial.Serial(port2, baud_rate)
+
+def forward_data(source, destination):
+    while True:
+        data = source.read()
+        if data:
+            destination.write(data)
+
+# Create threads for forwarding data
+thread1 = threading.Thread(target=forward_data, args=(ser1, ser2))
+thread2 = threading.Thread(target=forward_data, args=(ser2, ser1))
+
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Join the threads to keep the program running
+thread1.join()
+thread2.join()
