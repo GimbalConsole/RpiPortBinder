3514e6218256c1b60a17c4f7368c6fbdbbe388e7
admin <gimbaltest123@hotmail.com>
2024-07-19 01:57:26 +0800

    set path

--- setup_autorun.sh
+++ setup_autorun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="${BASH_SOURCE[0]}"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH=$PATH:"$SCRIPT_DIR"
 
 # Define the source and destination paths
